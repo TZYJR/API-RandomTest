e0d28f01efda31a9453fdf98890a9c759fe6ff0d
YangJiaQi <1295585769@qq.com>
2018-07-30 17:04:14 +0800

    1

---
+++ Assets/02.Scripts/01/RandomTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomTest : MonoBehaviour
+{
+    public GameObject Cube_a;
+
+    private void Update()
+    {
+        Instantiate(Cube_a, Random.onUnitSphere * 4.0f, Quaternion.identity);
+    }
+
+}
---
+++ Assets/02.Scripts/02/RandomTest_2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomTest_2 : MonoBehaviour
+{
+    Color[] colorArray;
+    public GameObject Cube;
+
+    void Statr()
+    {
+        //colorArray = new Color[3];
+        //colorArray[0] = Color.black;
+        //colorArray[1] = Color.red;
+        //colorArray[2] = Color.gray;
+    }
+
+    void Update()
+    {
+        colorArray = new Color[3];
+        colorArray[0] = Color.black;
+        colorArray[1] = Color.red;
+        colorArray[2] = Color.green;
+
+        //Vector3 random = Random.insideUnitSphere * 10;
+        //Instantiate(Cube, random, Quaternion.identity);
+        //Instantiate(Cube, Random.insideUnitSphere * 30f, Quaternion.identity);
+
+        //Instantiate(Cube, Random.onUnitSphere * 3f, Quaternion.identity);
+
+        Vector3 v3 = Random.onUnitSphere * 10;
+        GameObject currentCube = Instantiate(Cube, v3, Quaternion.identity) as GameObject;
+        currentCube.GetComponent<Renderer>().material.color = colorArray[Random.Range(0, 3)];
+
+    }
+
+}
